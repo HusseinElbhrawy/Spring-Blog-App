53a5eeb6a18721409479cb267413a061303d499d
Hussein Elbhrawy <hussein.elbhrawy74@gmail.com>
2024-10-12 16:46:40 +0300

    update login

--- src/main/java/com/husseinelbhrawy/BlogApp/Service/Implementation/AuthServiceImplementation.java
+++ src/main/java/com/husseinelbhrawy/BlogApp/Service/Implementation/AuthServiceImplementation.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -43,7 +44,8 @@ public class AuthServiceImplementation implements AuthService {
 
         //? authenticate method need Authentication in param , but Authentication is an interface, so we use UsernamePasswordAuthenticationToken
         Authentication authentication =    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getUsernameOrEmail(), loginDTO.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+        SecurityContext securityContext =   SecurityContextHolder. getContext();
+        securityContext.setAuthentication(authentication);
 
         return  ResponseEntity.ok("Login Successful");
     }
